6bf9b5b9cd573c21994a8c350f677abbddd89f1b
Sueun Cho <sueun.dev@gmail.com>
2023-08-20 19:22:01 -0400

    Update coindesk_sentiments_ai.py

--- coindesk_sentiments_ai.py
+++ coindesk_sentiments_ai.py
@@ -70,4 +70,4 @@ def get_results():
     return jsonify(analyzed_results)
 
 if __name__ == '__main__':
-    app.run(host= "localhost", port="3000", debug=True)
+    app.run(host="0.0.0.0", port="3000", debug=True)
